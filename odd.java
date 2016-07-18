67d4312e453bf5448f70dd1eb23b97043f224f4a
janarthanan05 <gethjan@gmail.com>
2016-07-18 12:26:36 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,16 @@
+public class Odd {
+	public static void main(String args[])
+	{
+		int z;
+		z=Integer.parseInt(args[0]);
+		if(z%2==0)
+		{
+			System.out.println(+z+" is an even number");
+		}
+		else
+		{
+			System.out.println(+z+" is an odd number");
+		}
+	}
+
+}
