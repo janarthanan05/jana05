3b3bbef21cc9aeb2b7aa743cdcf756f52aaf02b8
janarthanan05 <gethjan@gmail.com>
2016-07-23 14:04:15 +0530

    Create duplicate.java

---
+++ duplicate.java
@@ -0,0 +1,23 @@
+public static String removeDuplicate(String s)
+{
+    char [] temp = s.toCharArray();
+    int length =temp.length; 
+    for (int i=0;i<length;i++)
+    {
+        for (int j = i+1; j<length;j++)
+        {
+            if(temp[i]==temp[j])
+            {
+                int test =j;
+                for(int k=j+1; k<length ; k++)
+                {
+                    temp[test] = temp[k];
+                    test++;
+                }
+                length--;
+                j--;
+            }
+        }
+    }
+    return String.copyValueOf(temp).substring(0,length);
+}
