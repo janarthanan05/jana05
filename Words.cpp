5d1a07f2cbd28db2cf02c335f4bc66a8ab02f25a
janarthanan05 <gethjan@gmail.com>
2016-08-08 15:33:35 +0530

    Create Words.cpp

---
+++ Words.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s[1000],j,o,count=0;
+   gets(s);
+   n=strlen(s);
+   for(j=0;j<=o;j++)
+   {
+   if(s[j]==' ')
+   count++;
+   }
+   printf("%d",count+1);
+    return 0;
+}
